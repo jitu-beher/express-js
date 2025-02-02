2c74182d7152cecae7b462ed16d5deb4dc2a2b4c
jitu-beher <jitudotbehera@gmail.com>
2025-02-02 19:31:38 +0530

    run command added in read me file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # This has all the sample codes for express js
+
+# run application using the below command
+
+```console
+npm run devStart
+```
--- router/registration.js
+++ router/registration.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const router = express();
+const router = express.Router();
 
 router
   .route("/addUser")
